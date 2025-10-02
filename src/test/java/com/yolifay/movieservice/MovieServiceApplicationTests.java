989064b7740a47ae2728ae2b41db4b0db9d5f512
Singgih Pratama <sp.mastama@gmail.com>
2025-10-02 21:00:05 +0700

    init project movie service

---
+++ src/test/java/com/yolifay/movieservice/MovieServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yolifay.movieservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
